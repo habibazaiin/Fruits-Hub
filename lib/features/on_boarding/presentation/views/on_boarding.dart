fbcb670d2bff32812d70e7509c99292d919aff21
habibazaiin <zain.habiba0@gmail.com>
2025-10-28 17:48:43 +0300

    create on_boarding view and on boarding page view

--- lib/features/on_boarding/presentation/views/on_boarding.dart
+++ lib/features/on_boarding/presentation/views/on_boarding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/features/on_boarding/presentation/views/widgets/on_boarding_body.dart';
 
 class OnBoarding extends StatelessWidget {
   const OnBoarding({super.key});
@@ -6,6 +7,8 @@ class OnBoarding extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: const OnBoardingBody(),
+    );
   }
 }
