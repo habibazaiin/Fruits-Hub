2fad24d8226656131cf080a20fb460b2d1c1692a
habibazaiin <zain.habiba0@gmail.com>
2025-10-28 17:18:01 +0300

    splash view logic done

--- lib/core/helper_functions/on_generate_routes.dart
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/features/on_boarding/presentation/views/on_boarding.dart';
 import 'package:fruits_hub/features/splash/presentation/views/splash_view.dart';
 
 Route<dynamic> onGenerateRoutes(RouteSettings settings) {
@@ -7,6 +8,10 @@ Route<dynamic> onGenerateRoutes(RouteSettings settings) {
     case SplashView.routeName:
       return MaterialPageRoute(
         builder: (context) => const SplashView(),
+      );
+      case OnBoarding.routeName:
+      return MaterialPageRoute(
+        builder: (context) => const OnBoarding(),
       );
     // Define your routes here
     default:
---
+++ lib/features/on_boarding/presentation/views/on_boarding.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class OnBoarding extends StatelessWidget {
+  const OnBoarding({super.key});
+  static const String routeName = 'onBoarding';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
