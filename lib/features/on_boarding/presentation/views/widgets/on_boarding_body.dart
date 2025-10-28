139c5f15b53cda4660e4ac929511f2bcd00e5f4e
habibazaiin <zain.habiba0@gmail.com>
2025-10-28 21:52:31 +0300

    add dots indicator to on boarding page view

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_body.dart
@@ -1,4 +1,6 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/utils/app_color.dart';
 import 'package:fruits_hub/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart';
 
 class OnBoardingBody extends StatelessWidget {
@@ -6,6 +8,16 @@ class OnBoardingBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(children: [Expanded(child: OnBoardingPageView())]);
+    return Column(children: [
+      const Expanded(child: OnBoardingPageView()),
+      DotsIndicator(
+        dotsCount: 2,
+        position: 0,
+        decorator: DotsDecorator(
+          activeColor: AppColor.primaryColor,
+          color: AppColor.primaryColor.withAlpha(50),
+        ),
+      )
+    ]);
   }
 }
