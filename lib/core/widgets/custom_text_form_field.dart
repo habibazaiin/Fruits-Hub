a8b062adc0c904114697fec74c9cef06a7ffe2cc
habibazaiin <zain.habiba0@gmail.com>
2025-10-29 18:53:08 +0300

    create custom text form field and add it to login view

---
+++ lib/core/widgets/custom_text_form_field.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/utils/app_text_style.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField(
+      {super.key,
+      required this.hintText,
+      this.suffixIcon,
+      required this.keyboardType});
+  final String hintText;
+  final Widget? suffixIcon;
+  final TextInputType keyboardType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: TextFormField(
+        keyboardType: keyboardType,
+        decoration: InputDecoration(
+            border: buildOutlineInputBorder(),
+            focusedBorder: buildOutlineInputBorder(),
+            enabledBorder: buildOutlineInputBorder(),
+            suffixIcon: suffixIcon,
+            filled: true,
+            fillColor: const Color(0XFFF9FAFA),
+            hintText: hintText,
+            hintStyle:
+                AppTextStyle.bold13.copyWith(color: const Color(0XFF949D9E))),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildOutlineInputBorder() {
+    return const OutlineInputBorder(
+      borderSide: BorderSide(color: Color(0XFFE6E9E9)),
+      borderRadius: BorderRadius.all(
+        Radius.circular(8),
+      ),
+    );
+  }
+}
--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/utils/app_color.dart';
+import 'package:fruits_hub/core/utils/app_text_style.dart';
+import 'package:fruits_hub/core/widgets/custom_text_form_field.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 24,
+          ),
+          const CustomTextFormField(
+            hintText: 'البريد الالكترونى',
+            keyboardType: TextInputType.emailAddress,
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const CustomTextFormField(
+            hintText: 'كلمة المرور',
+            keyboardType: TextInputType.visiblePassword,
+            suffixIcon: Icon(
+              Icons.remove_red_eye,
+              color: Color(0XFFC9CECF),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Text(
+            'هل نسيت كلمة المرور؟',
+            style: AppTextStyle.semiBold13
+                .copyWith(color: AppColor.lightPrimaryColor),
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+        ],
+      ),
+    );
   }
 }
