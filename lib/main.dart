cb2511b5148ea84a8b2dd9605bbba35f9524ce5e
habibazaiin <zain.habiba0@gmail.com>
2025-10-29 16:26:59 +0300

    create shared preferences singleton

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/helper_functions/on_generate_routes.dart';
+import 'package:fruits_hub/core/services/shared_preferences_singleton.dart';
 import 'package:fruits_hub/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruits_hub/l10n/app_localizations.dart';
 
-void main() {
+
+void main() async {
+  await Prefs.init();
   runApp(const FruitsHub());
 }
 
