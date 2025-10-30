8e038882bfca1da01ff492d78f1484aa830dd16e
habibazaiin <zain.habiba0@gmail.com>
2025-10-30 15:42:37 +0300

    create have an account widget

---
+++ lib/features/auth/presentation/views/widgets/have_an_account_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/utils/app_color.dart';
+import 'package:fruits_hub/core/utils/app_text_style.dart';
+
+class HaveAnAccountWidget extends StatelessWidget {
+  const HaveAnAccountWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'تمتلك حساب بالفعل؟ ',
+          style:
+              AppTextStyle.semiBold16.copyWith(color: const Color(0XFF949D9E)),
+        ),
+        GestureDetector(
+          onTap: () {
+            Navigator.pushNamed(context, 'signUpView');
+          },
+          child: Text('تسجيل دخول',
+              style: AppTextStyle.semiBold16
+                  .copyWith(color: AppColor.primaryColor)),
+        ),
+      ],
+    );
+  }
+}
