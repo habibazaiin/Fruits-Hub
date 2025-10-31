a1a7ddc9c2865d72b4c752f5449608264a1a5d8c
habibazaiin <zain.habiba0@gmail.com>
2025-10-31 14:39:41 +0200

    provide create an account cubit tosign up view

--- lib/features/auth/presentation/views/sign_up_view.dart
+++ lib/features/auth/presentation/views/sign_up_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/services/service_locator.dart';
 import 'package:fruits_hub/core/widgets/custom_app_bar.dart';
+import 'package:fruits_hub/features/auth/domain/repos/auth_repo.dart';
+import 'package:fruits_hub/features/auth/presentation/manager/create_an_account_cubit/create_an_account_cubit.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/sign_up_view_body.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignUpView extends StatelessWidget {
   const SignUpView({super.key});
@@ -8,9 +12,12 @@ class SignUpView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: customAppBar(context, 'حساب جديد'),
-      body: const SignUpViewBody(),
+    return BlocProvider(
+      create: (context) => CreateAnAccountCubit(authRepo: getIt<AuthRepo>()),
+      child: Scaffold(
+        appBar: customAppBar(context, 'حساب جديد'),
+        body: const SignUpViewBody(),
+      ),
     );
   }
 }
