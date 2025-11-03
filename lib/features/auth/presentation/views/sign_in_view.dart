0d4c57035e8ac21bb56b56a15f72d0d67899deba
habibazaiin <zain.habiba0@gmail.com>
2025-11-03 13:30:26 +0200

    integrate sign in cubit

--- lib/features/auth/presentation/views/sign_in_view.dart
+++ lib/features/auth/presentation/views/sign_in_view.dart
@@ -5,6 +5,7 @@ import 'package:fruits_hub/core/widgets/custom_app_bar.dart';
 import 'package:fruits_hub/features/auth/domain/repos/auth_repo.dart';
 import 'package:fruits_hub/features/auth/presentation/manager/sign_in_cubit/sign_in_cubit.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/login_view_body.dart';
+import 'package:fruits_hub/features/auth/presentation/views/widgets/sign_in_view_body_bloc_consumer.dart';
 
 class SignInView extends StatelessWidget {
   const SignInView({super.key});
@@ -16,8 +17,9 @@ class SignInView extends StatelessWidget {
       create: (context) => SignInCubit(getIt<AuthRepo>()),
       child: Scaffold(
         appBar: customAppBar(context, 'تسجيل الدخول'),
-        body: const LoginViewBody(),
+        body: const SignInViewBodyBlocConsumer(),
       ),
     );
   }
 }
+
--- lib/features/auth/presentation/views/widgets/sign_up_view_body_bloc_consumer.dart
+++ lib/features/auth/presentation/views/widgets/sign_up_view_body_bloc_consumer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub/core/widgets/custom_loading_indicator.dart';
 import 'package:fruits_hub/core/helper_functions/custom_snack_bar.dart';
+import 'package:fruits_hub/core/widgets/custom_progress_hud.dart';
 import 'package:fruits_hub/features/auth/presentation/manager/sign_up_cubit/sign_up_cubit.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/sign_up_view_body.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
@@ -29,7 +30,7 @@ class SignUpViewBodyBlocConsumer extends StatelessWidget {
         }
       },
       builder: (context, state) {
-        return ModalProgressHUD(
+        return CustomProgressHud(
             inAsyncCall: state is SignUpLoading ? true : false,
             child: const SignUpViewBody());
       },
