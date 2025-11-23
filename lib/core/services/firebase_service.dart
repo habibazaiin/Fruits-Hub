7ec45cd873a6d176357a04451ce4f8c4b21efd58
habibazaiin <zain.habiba0@gmail.com>
2025-11-23 14:11:35 +0200

    implement add data method to firebase service

--- lib/core/services/firebase_service.dart
+++ lib/core/services/firebase_service.dart
@@ -5,6 +5,19 @@ import 'package:fruits_hub/core/errors/exception.dart';
 import 'package:fruits_hub/core/services/database_service.dart';
 
 class FirebaseService extends DatabaseService {
-  
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  @override
+  Future<void> addData(
+      {required String collectionPath,
+      required Map<String, dynamic> data}) async {
+    try {
+      await firestore
+          .collection(collectionPath)
+          .add(data);
+    } catch (e) {
+      log('FirebaseService - addData - Unexpected Error: ${e.toString()} & ecode ${e.hashCode}');
+      throw CustomException('حدث خطأ غير متوقع أثناء إضافة البيانات.');
+    }
+  }
   
 }
